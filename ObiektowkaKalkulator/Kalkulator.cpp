b2fd5e9e0e78a5822accbca1f1e3ae7454a7d06a
Jan Bieszczad <ridicoluslvl@gmail.com>
2025-03-23 17:27:22 +0100

    Mam nadzieje ze tak mozna

--- ObiektowkaKalkulator/Kalkulator.cpp
+++ ObiektowkaKalkulator/Kalkulator.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 using namespace std;
 
-double memory; //pamięć kalkulatora
-bool mem_used; //
+double memory = 0; //pamięć kalkulatora
+bool mem_used = false; //
 
 int Menu() {
     cout << "Choose action: \n"
@@ -24,7 +24,7 @@ double add(double a, double b) {
     }
     return a + b;
 }
-double subtract(double a , double b) {
+double subtract(double a, double b) {
     if (mem_used)
         a = memory;
     return a - b;
@@ -39,84 +39,89 @@ double divide(double a, double b) {
         a = memory;
     return a / b;
 }
-double modulo(double a, double b){
+double modulo(double a, double b) {
     if (mem_used)
         a = memory;
     return a - (int)(a / b) * b;
 }
-void erase(double & memory) {
+void erase(double& memory) {
     memory = 0;
 }
 void CalculatorBody() {
     int a, b;
     switch (Menu()) {
-        case 1: {
-            if (!mem_used) {
-                cout << "Give numbers: ";
-                cin >> a >> b;
-            }
-            else {
-                cout << "Give number: ";
-                cin >> b;
-            }
-             cout << add(a, b);
-             memory += add(a, b);
+    case 1: {
+        if (!mem_used) {
+            cout << "Give numbers: ";
+            cin >> a >> b;
         }
-        case 2: {
-            if (!mem_used) {
-                cout << "Give numbers: ";
-                cin >> a >> b;
-            }
-            else {
-                cout << "Give number: ";
-                cin >> b;
-            }
-            cout << subtract(a, b);
-            memory += subtract(a, b);
+        else {
+            cout << "Give number: ";
+            cin >> b;
         }
-        case 3: {
-            if (!mem_used) {
-                cout << "Give numbers: ";
-                cin >> a >> b;
-            }
-            else {
-                cout << "Give number: ";
-                cin >> b;
-            }
-            cout << multiply(a, b);
-            memory += multiply(a, b);
+        cout << add(a, b);
+        memory += add(a, b);
+        break;
+    }
+    case 2: {
+        if (!mem_used) {
+            cout << "Give numbers: ";
+            cin >> a >> b;
         }
-        case 4: {
-            if (!mem_used) {
-                cout << "Give numbers: ";
-                cin >> a >> b;
-            }
-            else {
-                cout << "Give number: ";
-                cin >> b;
-            }
-            cout << divide(a, b);
-            memory += divide(a, b);
+        else {
+            cout << "Give number: ";
+            cin >> b;
         }
-        case 5: {
-            if (!mem_used) {
-                cout << "Give numbers: ";
-                cin >> a >> b;
-            }
-            else {
-                cout << "Give number: ";
-                cin >> b;
-            }
-            cout << modulo(a, b);
-            memory += modulo(a, b);
+        cout << subtract(a, b);
+        memory += subtract(a, b);
+        break;
+    }
+    case 3: {
+        if (!mem_used) {
+            cout << "Give numbers: ";
+            cin >> a >> b;
         }
+        else {
+            cout << "Give number: ";
+            cin >> b;
+        }
+        cout << multiply(a, b);
+        memory += multiply(a, b);
+        break;
+    }
+    case 4: {
+        if (!mem_used) {
+            cout << "Give numbers: ";
+            cin >> a >> b;
+        }
+        else {
+            cout << "Give number: ";
+            cin >> b;
+        }
+        cout << divide(a, b);
+        memory += divide(a, b);
+        break;
+    }
+    case 5: {
+        if (!mem_used) {
+            cout << "Give numbers: ";
+            cin >> a >> b;
+        }
+        else {
+            cout << "Give number: ";
+            cin >> b;
+        }
+        cout << modulo(a, b);
+        memory += modulo(a, b);
+        break;
+    }
     }
 }
 
-int main()//błędy: delenie przez 0 i modulo przez 0
+int main()//błędy: delenie przez 0 i modulo przez 0hjhj
 {
     //while (true) {
-        CalculatorBody();
+    CalculatorBody();
 
     //}
 }
