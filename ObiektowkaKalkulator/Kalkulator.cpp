b2e5ad490a4ff713311d7ea883a33353615d7047
Jan Bieszczad <ridicoluslvl@gmail.com>
2025-03-23 18:22:10 +0100

    Dodano obsługę błędów

--- ObiektowkaKalkulator/Kalkulator.cpp
+++ ObiektowkaKalkulator/Kalkulator.cpp
@@ -12,6 +12,7 @@ int Menu() {
         << "4) /\n"
         << "5) %\n"
         << "6) Clear memory\n"
+		<< "7) Show memory\n"
         << "ENTER - exit\n";
     int number;
     cin >> number;
@@ -47,81 +48,86 @@ double modulo(double a, double b) {
 void erase(double& memory) {
     memory = 0;
 }
+double memory_used(double & memory) {
+	if (memory != 0) {
+		cout << "Memory used: " << memory << endl;
+		return memory;
+	}
+	else {
+		cout << "Memory is empty\nGive the first number: ";
+		return cin >> a;
+	}
+}
 void CalculatorBody() {
     int a, b;
     switch (Menu()) {
-    case 1: {
-        if (!mem_used) {
-            cout << "Give numbers: ";
-            cin >> a >> b;
-        }
-        else {
+        case 1: {
+			a = memory_used(memory);
             cout << "Give number: ";
             cin >> b;
+            cout << add(a, b) << " (added to memory)\n";
+            memory += add(a, b);
+            break;
         }
-        cout << add(a, b);
-        memory += add(a, b);
-        break;
-    }
-    case 2: {
-        if (!mem_used) {
-            cout << "Give numbers: ";
-            cin >> a >> b;
-        }
-        else {
+        case 2: {
+            a = memory_used(memory);
             cout << "Give number: ";
             cin >> b;
+            cout << subtract(a, b) << " (added to memory)\n";
+            memory += subtract(a, b);
+            break;
         }
-        cout << subtract(a, b);
-        memory += subtract(a, b);
-        break;
-    }
-    case 3: {
-        if (!mem_used) {
-            cout << "Give numbers: ";
-            cin >> a >> b;
-        }
-        else {
+        case 3: {
+            a = memory_used(memory);
             cout << "Give number: ";
             cin >> b;
+            cout << multiply(a, b) << " (added to memory)\n";
+            memory += multiply(a, b);
+            break;
         }
-        cout << multiply(a, b);
-        memory += multiply(a, b);
-        break;
-    }
-    case 4: {
-        if (!mem_used) {
-            cout << "Give numbers: ";
-            cin >> a >> b;
-        }
-        else {
+        case 4: {
+            a = memory_used(memory);
             cout << "Give number: ";
             cin >> b;
+            if (b == 0) {
+				cout << "Error: division by 0\n";
+				break;
+            }
+            cout << divide(a, b) << " (added to memory)\n";
+            memory += divide(a, b);
+            break;
         }
-        cout << divide(a, b);
-        memory += divide(a, b);
-        break;
-    }
-    case 5: {
-        if (!mem_used) {
-            cout << "Give numbers: ";
-            cin >> a >> b;
-        }
-        else {
+        case 5: {
+            a = memory_used(memory);
             cout << "Give number: ";
             cin >> b;
+            if (b == 0) {
+				cout << "Error: modulo by 0\n";
+                break;
+            }
+            cout << modulo(a, b) << " (added to memory)\n";
+            memory += modulo(a, b);
+            break;
         }
-        cout << modulo(a, b);
-        memory += modulo(a, b);
-        break;
-    }
+        case 6: {
+			erase(memory);
+			cout << "Memory cleared\n";
+			break;
+        }
+        case 7: {
+			cout << "In current memory: " << memory << endl;
+        }
+        default: {
+            cout << "Ending program...\n";
+            break;
+        }
     }
 }
 
-int main()//błędy: delenie przez 0 i modulo przez 0jhjh
+int main()//błędy: delenie przez 0 i modulo przez 0
 {
-    //while (true) {
-    CalculatorBody();
+    while (true) {
+        CalculatorBody();
 
-    //}
+    }
 }
